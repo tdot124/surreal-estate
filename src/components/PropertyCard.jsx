a1591107afff67298c12b9a507e7a581a7b45f10
Tom Mills <t.mills124@gmail.com>
2022-10-09 19:11:24 +0100

    Added some styling to PropertyCard

--- src/components/PropertyCard.jsx
+++ src/components/PropertyCard.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBed,
+  faBath,
+  faSterlingSign,
+  faEnvelope,
+} from "@fortawesome/free-solid-svg-icons";
 import "../styles/property-card.css";
 
 const PropertyCard = ({
@@ -15,11 +22,29 @@ const PropertyCard = ({
     <div className="property-card">
       <div className="property-card-title">{title}</div>
       <div className="property-card-type">{type}</div>
-      <div className="property-card-bathrooms">{bathrooms}</div>
-      <div className="property-card-bedrooms">{bedrooms}</div>
-      <div className="property-card-price">{price}</div>
+      <div className="property-card-bathrooms">
+        <span className="property-card-bathrooms-icon">
+          <FontAwesomeIcon icon={faBath} />
+        </span>
+        {bathrooms}
+      </div>
+      <div className="property-card-bedrooms">
+        <span className="property-card-bedrooms-icon">
+          <FontAwesomeIcon icon={faBed} />
+        </span>
+        {bedrooms}
+      </div>
+      <div className="property-card-price">
+        <span className="property-card-price-icon">
+          <FontAwesomeIcon icon={faSterlingSign} />
+        </span>
+        {price}
+      </div>
       <div className="property-card-city">{city}</div>
       <a href={`mailto:${email}`} className="property-card-email">
+        <span className="property-card-email-icon">
+          <FontAwesomeIcon icon={faEnvelope} />
+        </span>
         Email
       </a>
     </div>
